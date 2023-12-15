7b2bfb88595f0bb5d5d6f86d95316985e6db7642
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-12-15 14:43:38 -0500

    making pacman its own component, updating position,

---
+++ pacman/src/02PacMan.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+
+const PacMan = ({ boardData, pacmanPosition, setPacmanPosition, setBoardData }) => {
+  const handleKeyDown = (event) => {
+    const { key } = event;
+    const { row, col } = pacmanPosition;
+
+    // new position based on key press
+    let newRow = row;
+    let newCol = col;
+
+    switch (key) {
+      case 'ArrowUp':
+        newRow = row - 1;
+        break;
+      case 'ArrowDown':
+        newRow = row + 1;
+        break;
+      case 'ArrowLeft':
+        newCol = col - 1;
+        break;
+      case 'ArrowRight':
+        newCol = col + 1;
+        break;
+      default:
+        return;
+    }
+
+    if (boardData[newRow] && boardData[newRow][newCol] !== 'X') {
+      const newBoardData = [...boardData];
+      newBoardData[row][col] = '.';
+      newBoardData[newRow][newCol] = 'P';
+      setBoardData(newBoardData);
+      setPacmanPosition({ row: newRow, col: newCol });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [boardData, pacmanPosition, setPacmanPosition, setBoardData]);
+
+  return null; // placeholder for image if i want to add
+};
+
+export default PacMan;
